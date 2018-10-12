56a06ee28b4ef7fbe1640201d10563809f4d08c8
Lekrieg <danielhenriquesilva2010@gmail.com>
2018-10-12 14:01:38 -0300

    Inicio do trabalho

---
+++ TrabalhoPOO3/TrabalhoPOO3/Teste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoPOO3
+{
+    public partial class Teste : Form
+    {
+        public Teste()
+        {
+            InitializeComponent();
+        }
+
+        private void Teste_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+//por email até o dia 8
+//enviar o codigo, os documentos e a apresentação montada
